07027bbd3e9bf5524ebd5b4129f3cc9d3c327d31
maspriyambodo <maspriyambodo@gmail.com>
2018-04-25 03:43:54 +0700

    Remove Web Config

--- application/config_web/autoload.php
+++
@@ -1,1 +0,0 @@
-<?php defined('BASEPATH')OR exit('No direct script access allowed');$autoload['packages']=array();$autoload['libraries']=array('session','database');$autoload['drivers']=array();$autoload['helper']=array('form','url','html');$autoload['config']=array();$autoload['language']=array();$autoload['model']=array();
