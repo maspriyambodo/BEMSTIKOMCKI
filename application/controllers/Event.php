d3dc7ba95efa699e7c7a69cc5bee48be4aef472b
Priyambodo <maspriyambodo@gmail.com>
2018-09-04 17:57:15 +0700

    edited by priyambodo

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -1,23 +1,29 @@
-<?php 
+<?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Event extends CI_Controller{
-    function __construct(){
+
+class Event extends CI_Controller {
+
+    function __construct() {
         parent::__construct();
         $this->load->helper('text');
         $this->load->library('pagination');
     }
-    function paswa(){
+
+    function paswa() {
         $newdata = array(
             'TitlePage' => " PASWA 2018 - BEM STIKOM CKI " . date("Y"),
         );
-        $this->load->view('vPaswa',$newdata);
+        $this->load->view('vPaswa', $newdata);
     }
-    function external(){
+
+    function external() {
         $newdata = array(
             'TitlePage' => "CKI competition External ",
         );
-        $this->load->view('vExternal',$newdata);
+        $this->load->view('vExternal', $newdata);
     }
-    
+
 }
+
 ?>
